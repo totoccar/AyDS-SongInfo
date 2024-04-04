2597cdc50f1688ac4cf9ae2068d85c8e50384063
TobiasThiessen11 <tobiasthiessen@live.com.ar>
2024-04-03 22:59:29 -0300

    Cambios en home para el releaseDatePrecision

--- app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
+++ app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
@@ -7,12 +7,13 @@ sealed class Song {
         val artistName: String,
         val albumName: String,
         val releaseDate: String,
+        val releaseDatePrecision: String,
         val spotifyUrl: String,
         val imageUrl: String,
         var isLocallyStored: Boolean = false
     ) : Song() {
 
-        val year: String = releaseDate.split("-").first()
+        val year: String = releaseDatePrecision
     }
 
     object EmptySong : Song()
