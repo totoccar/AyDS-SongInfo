1bc435c9523011f2d99929c45fe8049871b8c8c2
Antonio <totocarlos0@outlook.com>
2024-04-19 09:29:44 -0300

    Corregimos Etapa 2 - Iniciamos Etapa 3

--- app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
+++ app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
@@ -14,39 +14,7 @@ sealed class Song {
         val spotifyUrl: String,
         val imageUrl: String,
         var isLocallyStored: Boolean = false
-    ) : Song() {
-        fun showReleaseDate():String{
-            val listDate = releaseDate.split("-").map{it.toInt() }
-            return when (releaseDatePrecision){
-                "day"-> return dayReleaseDate(listDate)
-                "month"-> return monthReleaseDate(listDate)
-                "year"-> return yearReleaseDate(listDate)
-                else -> "invalidReleaseDateException"
-            }
-        }
-        private fun dayReleaseDate(listDate: List<Int>): String{
-            return  "${listDate[2]}/${listDate[1]}/${listDate[0]}"
-        }
-
-        private fun monthReleaseDate(listDate: List<Int>): String{
-            val month= Month.of(listDate[1]).toString()
-            return "${month},${listDate[0]}"
-        }
-
-        private fun yearReleaseDate(listDate: List<Int>): String{
-            return "${listDate[0]} ${leapYear(listDate[0])}"
-        }
-
-        private fun leapYear(year: Int): String{
-            return if (Year.of(year).isLeap)
-                "(leap year)"
-            else
-                "(not a leap year)"
-        }
-
-    }
-
+    ) : Song()
 
     object EmptySong : Song()
 }
-
--- app/src/main/java/ayds/songinfo/home/view/HomeViewInjector.kt
+++ app/src/main/java/ayds/songinfo/home/view/HomeViewInjector.kt
@@ -1,11 +1,10 @@
 package ayds.songinfo.home.view
-
 import ayds.songinfo.home.controller.HomeControllerInjector
 import ayds.songinfo.home.model.HomeModelInjector
 
 object HomeViewInjector {
 
-    val songDescriptionHelper: SongDescriptionHelper = SongDescriptionHelperImpl()
+    val songDescriptionHelper: SongDescriptionHelper = SongDescriptionHelperImpl(ReleaseDateResolverFactoryImpl())
 
     fun init(homeView: HomeView) {
         HomeModelInjector.initHomeModel(homeView)
